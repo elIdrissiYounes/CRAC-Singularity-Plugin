41f47790b5af0d8dd256981414e0715704003706
elIdrissiYounes <elidrissiyazami.y@husky.neu.edu>
2020-09-29 22:41:11 -0400

    restart script

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-latest=$( ls -t1 ./lancium-checkpoint/ | grep .dmtcp |  head -n 1 )
+latest=$( ls -t1 ./lancium-checkpoint/ | grep "[.]dmtcp" |  head -n 1 )
 dmtcp_restart --cuda --new-coordinator "./lancium-checkpoint/$latest"
